846b168582df13742efad13006452d8e99446bd6
Justin Bender <jrkbender@gmail.com>
2020-05-14 11:18:55 -0400

    expanding on some dictionay challenges

--- script.py
+++ script.py
@@ -181,3 +181,137 @@ print('lol', player_to_words)
 print('\n')
 
 print(player_to_points)
+
+# Write your sum_values function here:
+def sum_values(my_dictionary):
+  total_value = 0
+  for value in my_dictionary.values():
+    total_value += value
+  return total_value
+
+# Uncomment these function calls to test your sum_values function:
+print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+# should print 10
+print(sum_values({10:1, 100:2, 1000:3}))
+# should print 6
+
+# Write your sum_even_keys function here:
+def sum_even_keys(my_dictionary):
+  total_evens_sum = 0
+  #sum of values of even keys
+  for key, value in my_dictionary.items():
+    if key % 2 is 0:
+      total_evens_sum += value
+  return total_evens_sum
+# Uncomment these function calls to test your  function:
+print(sum_even_keys({1:5, 2:2, 3:3}))
+# should print 2
+print(sum_even_keys({10:1, 100:2, 1000:3}))
+# should print 6
+
+# Write your add_ten function here:
+def add_ten(my_dictionary):
+  for key in my_dictionary.keys():
+    my_dictionary[key] += 10
+  return my_dictionary
+
+# Uncomment these function calls to test your  function:
+print(add_ten({1:5, 2:2, 3:3}))
+# should print {1:15, 2:12, 3:13}
+print(add_ten({10:1, 100:2, 1000:3}))
+# should print {10:11, 100:12, 1000:13}
+
+# Write your values_that_are_keys function here:
+def values_that_are_keys(my_dictionary):
+  values_that_are_keys = []
+  keys = my_dictionary.keys()
+  values = my_dictionary.values()
+  for value in values:
+    if value in keys:
+      values_that_are_keys.append(value)
+  return values_that_are_keys
+# Uncomment these function calls to test your  function:
+print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+# should print [1, 4]
+print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+# should print ["a"]
+
+# Write your max_key function here:
+def max_key(my_dictionary):
+  largest_key = float("-inf")
+  largest_value = float("-inf")
+  for key, value in my_dictionary.items():
+    if value > largest_value:
+      largest_value = value
+      largest_key = key
+  return largest_key
+
+# Uncomment these function calls to test your  function:
+print(max_key({1:100, 2:1, 3:4, 4:10}))
+# should print 1
+print(max_key({"a":100, "b":10, "c":1000}))
+# should print "c"
+
+# Write your word_length_dictionary function here:
+def word_length_dictionary(words):
+  #key value pairs 
+  #key is the word
+  word_lengths = []
+  #value is the length of words
+  for i in range(len(words)):
+    word_lengths.append(len(words[i]))
+  word_length_dictionary = {key:value for key, value in zip(words, word_lengths)}
+  return word_length_dictionary
+# Uncomment these function calls to test your  function:
+print(word_length_dictionary(["apple", "dog", "cat"]))
+# should print {"apple":5, "dog": 3, "cat":3}
+print(word_length_dictionary(["a", ""]))
+# should print {"a": 1, "": 0}
+
+# Write your frequency_dictionary function here:
+def frequency_dictionary(words):
+  freq_dictionary = {}
+  for word in words:
+    if word in freq_dictionary.keys():
+      freq_dictionary[word] += 1
+    else:
+      freq_dictionary[word] = 1
+  return freq_dictionary
+# Uncomment these function calls to test your  function:
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
+# should print {"apple":2, "cat":1, 1:1}
+print(frequency_dictionary([0,0,0,0,0]))
+# should print {0:5}
+
+# Write your unique_values function here:
+def unique_values(my_dictionary):
+  seen_values = []
+  for value in my_dictionary.values():
+    if value not in seen_values:
+      seen_values.append(value)
+  return len(seen_values)
+
+# Uncomment these function calls to test your  function:
+print(unique_values({0:3, 1:1, 4:1, 5:3}))
+# should print 2
+print(unique_values({0:3, 1:3, 4:3, 5:3}))
+# should print 1
+
+# Write your count_first_letter function here:
+def count_first_letter(names):
+  
+  new_dict = {}
+  for key, values in names.items():
+    if key[0] in new_dict:
+      new_dict[key[0]] += len(values)
+    else:
+      new_dict[key[0]] = len(values)
+# return a new dictionary where each key = first letter last name
+# value = # people whose last name begins with that letter.
+  return new_dict
+
+# Uncomment these function calls to test your  function:
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 4, "L": 3}
+print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Sannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 7}
